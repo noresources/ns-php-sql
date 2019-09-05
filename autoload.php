bbd4d4a2bc82af8f2aba09e50e0772547c4c7c84
Renaud Guillard <dev@nore.fr>
2019-09-05 20:46:00 +0200

    move files to subfolders

--- autoload.php
+++ autoload.php
@@ -10,34 +10,28 @@ spl_autoload_register(function($className) {
 		require_once(__DIR__ . '/SQLite/PreparedStatement.php');
 	} elseif ($className == 'NoreSources\SQL\SQLite\Recordset') {
 		require_once(__DIR__ . '/SQLite/Recordset.php');
-	} elseif ($className == 'NoreSources\SQL\StructureException') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\StructureElement') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\TableColumnStructure') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\TableStructure') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\TableSetStructure') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\DatasourceStructure') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\StructureResolverException') {
-		require_once(__DIR__ . '/Structures.php');
-	} elseif ($className == 'NoreSources\SQL\StructureResolver') {
-		require_once(__DIR__ . '/Structures.php');
 	} elseif ($className == 'NoreSources\SQL\StatementException') {
 		require_once(__DIR__ . '/Statements/Base.php');
 	} elseif ($className == 'NoreSources\SQL\TableReference') {
 		require_once(__DIR__ . '/Statements/Base.php');
 	} elseif ($className == 'NoreSources\SQL\Statement') {
 		require_once(__DIR__ . '/Statements/Base.php');
+	} elseif ($className == 'NoreSources\SQL\StatementContextParameter') {
+		require_once(__DIR__ . '/Statements/Context.php');
+	} elseif ($className == 'NoreSources\SQL\StatementContextParameterMap') {
+		require_once(__DIR__ . '/Statements/Context.php');
+	} elseif ($className == 'NoreSources\SQL\StatementContext') {
+		require_once(__DIR__ . '/Statements/Context.php');
 	} elseif ($className == 'NoreSources\SQL\ResultColumnReference') {
 		require_once(__DIR__ . '/Statements/Select.php');
 	} elseif ($className == 'NoreSources\SQL\JoinClause') {
 		require_once(__DIR__ . '/Statements/Select.php');
 	} elseif ($className == 'NoreSources\SQL\SelectQuery') {
 		require_once(__DIR__ . '/Statements/Select.php');
+	} elseif ($className == 'NoreSources\SQL\StatementBuilder') {
+		require_once(__DIR__ . '/Statements/Builder.php');
+	} elseif ($className == 'NoreSources\SQL\GenericStatementBuilder') {
+		require_once(__DIR__ . '/Statements/Builder.php');
 	} elseif ($className == 'NoreSources\SQL\InsertQuery') {
 		require_once(__DIR__ . '/Statements/Insert.php');
 	} elseif ($className == 'NoreSources\SQL\DropTableQuery') {
@@ -46,44 +40,50 @@ spl_autoload_register(function($className) {
 		require_once(__DIR__ . '/Statements/Update.php');
 	} elseif ($className == 'NoreSources\SQL\CreateTableQuery') {
 		require_once(__DIR__ . '/Statements/CreateTable.php');
-	} elseif ($className == 'NoreSources\SQL\StatementContextParameter') {
-		require_once(__DIR__ . '/StatementContext.php');
-	} elseif ($className == 'NoreSources\SQL\StatementContextParameterMap') {
-		require_once(__DIR__ . '/StatementContext.php');
-	} elseif ($className == 'NoreSources\SQL\StatementContext') {
-		require_once(__DIR__ . '/StatementContext.php');
+	} elseif ($className == 'NoreSources\SQL\ParameterArray') {
+		require_once(__DIR__ . '/Statements/PreparedStatement.php');
+	} elseif ($className == 'NoreSources\SQL\PreparedStatement') {
+		require_once(__DIR__ . '/Statements/PreparedStatement.php');
 	} elseif ($className == 'NoreSources\SQL\Constants') {
 		require_once(__DIR__ . '/Constants.php');
-	} elseif ($className == 'NoreSources\SQL\StructureSerializer') {
-		require_once(__DIR__ . '/StructureSerializer.php');
-	} elseif ($className == 'NoreSources\SQL\JSONStructureSerializer') {
-		require_once(__DIR__ . '/StructureSerializer.php');
-	} elseif ($className == 'NoreSources\SQL\XMLStructureSerializer') {
-		require_once(__DIR__ . '/StructureSerializer.php');
-	} elseif ($className == 'NoreSources\SQL\StatementBuilder') {
-		require_once(__DIR__ . '/StatementBuilder.php');
-	} elseif ($className == 'NoreSources\SQL\GenericStatementBuilder') {
-		require_once(__DIR__ . '/StatementBuilder.php');
 	} elseif ($className == 'NoreSources\SQL\ConnectionException') {
 		require_once(__DIR__ . '/Connection.php');
 	} elseif ($className == 'NoreSources\SQL\Connection') {
 		require_once(__DIR__ . '/Connection.php');
 	} elseif ($className == 'NoreSources\SQL\ConnectionHelper') {
 		require_once(__DIR__ . '/Connection.php');
+	} elseif ($className == 'NoreSources\SQL\StructureException') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\StructureElement') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\TableColumnStructure') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\TableStructure') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\TableSetStructure') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\DatasourceStructure') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\StructureResolverException') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\StructureResolver') {
+		require_once(__DIR__ . '/Structure/Structures.php');
+	} elseif ($className == 'NoreSources\SQL\StructureSerializer') {
+		require_once(__DIR__ . '/Structure/Serializers.php');
+	} elseif ($className == 'NoreSources\SQL\JSONStructureSerializer') {
+		require_once(__DIR__ . '/Structure/Serializers.php');
+	} elseif ($className == 'NoreSources\SQL\XMLStructureSerializer') {
+		require_once(__DIR__ . '/Structure/Serializers.php');
 	} elseif ($className == 'NoreSources\SQL\TableConstraint') {
-		require_once(__DIR__ . '/TableConstraints.php');
+		require_once(__DIR__ . '/Structure/TableConstraints.php');
 	} elseif ($className == 'NoreSources\SQL\ColumnTableConstraint') {
-		require_once(__DIR__ . '/TableConstraints.php');
+		require_once(__DIR__ . '/Structure/TableConstraints.php');
 	} elseif ($className == 'NoreSources\SQL\PrimaryKeyTableConstraint') {
-		require_once(__DIR__ . '/TableConstraints.php');
+		require_once(__DIR__ . '/Structure/TableConstraints.php');
 	} elseif ($className == 'NoreSources\SQL\UniqueTableConstraint') {
-		require_once(__DIR__ . '/TableConstraints.php');
+		require_once(__DIR__ . '/Structure/TableConstraints.php');
 	} elseif ($className == 'NoreSources\SQL\ForeignKeyTableConstraint') {
-		require_once(__DIR__ . '/TableConstraints.php');
-	} elseif ($className == 'NoreSources\SQL\ParameterArray') {
-		require_once(__DIR__ . '/PreparedStatement.php');
-	} elseif ($className == 'NoreSources\SQL\PreparedStatement') {
-		require_once(__DIR__ . '/PreparedStatement.php');
+		require_once(__DIR__ . '/Structure/TableConstraints.php');
 	} elseif ($className == 'NoreSources\SQL\Expression') {
 		require_once(__DIR__ . '/Expressions/Base.php');
 	} elseif ($className == 'NoreSources\SQL\PreformattedExpression') {
