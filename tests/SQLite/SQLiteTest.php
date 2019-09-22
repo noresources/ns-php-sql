fedceda5f7dfaba6faed17ed608f5e8e938f7242
Renaud Guillard <dev@nore.fr>
2019-09-22 20:55:03 +0200

    SQLite insert / select test

--- tests/SQLite/SQLiteTest.php
+++ tests/SQLite/SQLiteTest.php
@@ -27,14 +27,57 @@ final class SQLiteTest extends TestCase
 
 		$this->assertTrue($this->createDatabase(), 'Create SQLite file');
 
-		$this->derivedFileManager->setPersistent($this->sqliteFile, true);
-
 		$this->assertTrue($this->createTable($tableStructure), 'Create table ' .
 			$tableStructure->getPath());
 
 		//$this->assertTrue(false, 'Abort');
 
-		$this->derivedFileManager->setPersistent($this->sqliteFile, false);
+		$statement = new InsertQuery($tableStructure);
+		$statement['gender'] = 'M';
+		$statement('name', ':nameValue');
+		$statement('salary', ':salaryValue');
+
+		$statement = ConnectionHelper::prepareStatement($this->connection, $statement, $tableStructure);
+		
+		$this->assertInstanceOf(PreparedStatement::class, $statement);
+
+		$sql = strval($statement);
+		$sql = \SqlFormatter::format(strval($sql), false);
+		$this->derivedFileManager->assertDerivedFile($sql, __METHOD__, 'insert', 'sql');
+
+		$p = new ParameterArray();
+
+		$p->set('nameValue', 'Bob');
+		$p->set('salaryValue', 2000);
+		$result = $this->connection->executeStatement($statement, $p);
+		$this->assertInstanceOf(Recordset::class, $result);
+
+		$p->set('nameValue', 'Ron');
+		$result = $this->connection->executeStatement($statement, $p);
+		$this->assertInstanceOf(Recordset::class, $result);
+
+		$statement = new SelectQuery($tableStructure);
+		$statement->columns('name', 'gender', 'salary');
+		$statement = ConnectionHelper::prepareStatement($this->connection, $statement, $tableStructure);
+		$result = $this->connection->executeStatement($statement);
+		$this->assertInstanceOf(Recordset::class, $result);
+		
+		$expected = [
+				['name' => 'Bob', 'gender' => 'M', 'salary' => 2000.],
+				['name' => 'Ron', 'gender' => 'M', 'salary' => 2000.]
+		];
+
+		$index = 0;
+		foreach ($result as $row)
+		{
+			foreach ($expected[$index] as $name => $value)
+			{
+				$this->assertEquals($value, $row[$name], 'Row ' . $index . ' column ' .
+					$name);
+			}
+
+			$index++;
+		}
 	}
 
 	private function createTable(TableStructure $tableStructure)
@@ -58,23 +101,26 @@ final class SQLiteTest extends TestCase
 		if ($this->connection instanceof Connection)
 			return true;
 
-		$this->sqliteFile = $this->derivedFileManager->registerDerivedFile('SQLite', __METHOD__, 'db', 'sqlite');
+		$sqliteFile = $this->derivedFileManager->registerDerivedFile('SQLite', __METHOD__, 'db', 'sqlite');
+
+		if (\file_exists($sqliteFile))
+			unlink($sqliteFile);
 			
 		$this->connection = ConnectionHelper::createConnection([
 				K::CONNECTION_PARAMETER_CREATE => true,
 				K::CONNECTION_PARAMETER_SOURCE => [
-						'ns_unittests' => $this->sqliteFile
+						'ns_unittests' => $sqliteFile
 				],
 				K::CONNECTION_PARAMETER_TYPE => SQLite\Connection::class
 		]);
 		
 		$this->assertInstanceOf(SQLite\Connection::class, $this->connection, 'Create connection');
-		
+
+		$this->derivedFileManager->setPersistent($sqliteFile, true);
+
 		return true;
 	}
-	
-	private $sqliteFile;
-	
+
 	/**
 	 * @var DatasourceManager
 	 */
---
+++ tests/reference/SQLite_ParametersEmployees_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO "ns_unittests"."Employees" ("gender", "name", "salary") 
+VALUES 
+  ('M', :nameValue, :salaryValue)
