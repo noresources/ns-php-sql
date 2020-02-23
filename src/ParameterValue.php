edb9d9924b00d6c56b0a244300c791b303ccb9ec
Renaud Guillard <dev@nore.fr>
2020-02-23 19:11:55 +0100

    ParameterValue uses Value::dataTypeFromValue()

--- src/ParameterValue.php
+++ src/ParameterValue.php
@@ -10,6 +10,7 @@
 namespace NoreSources\SQL;
 
 use NoreSources\SQL\Constants as K;
+use NoreSources\SQL\Expression\Value;
 
 /**
  * Value of statement parameter.
@@ -43,19 +44,6 @@ class ParameterValue
 	public function __construct($value, $type = K::DATATYPE_UNDEFINED)
 	{
 		if ($type == K::DATATYPE_UNDEFINED)
-		{
-			if (\is_integer($value))
-				$type = K::DATATYPE_INTEGER;
-			elseif (\is_float($value))
-				$type = K::DATATYPE_FLOAT;
-			elseif (\is_bool($value))
-				$type = K::DATATYPE_BOOLEAN;
-			elseif (\is_null($value))
-				$type = K::DATATYPE_NULL;
-			elseif ($value instanceof \DateTime)
-				$type = K::DATATYPE_TIMESTAMP;
-			if (\is_string($value))
-				$type = K::DATATYPE_STRING;
-		}
+			$type = Value::dataTypeFromValue($value);
 	}
 }
